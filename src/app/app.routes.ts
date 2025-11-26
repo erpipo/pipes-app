5808adfa9a9bf3426cec36b6b3068cb7b78dd89f
Víctor Cano <victor.canomartin@kyndryl.com>
2025-11-26 15:17:43 +0100

    carga inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'basic',
+        title: 'pipes básicos',
+        loadComponent: () => import('./pages/basic-page/basic-page'),
+    },
+    {
+        path: 'custom',
+        title: 'pipes custom',
+        loadComponent: () => import('./pages/custom-page/custom-page'),
+    },
+    {
+        path: 'numeric',
+        title: 'pipes numéricos',
+        loadComponent: () => import('./pages/numbers-page/numbers-page'),
+    },
+    {
+        path: 'uncommon',
+        title: 'pipes poco comunes',
+        loadComponent: () => import('./pages/uncommon-page/uncommon-page'),
+    },
+    {
+        path: '**',
+        redirectTo: 'basic'
+    }
+];
