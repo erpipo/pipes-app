25699973d54197bdbb73a75878593d1ffb6ec803
Víctor Cano <victor.canomartin@kyndryl.com>
2025-12-02 11:36:38 +0100

    Pipes básicos y numéricos, planteo cambio de idioma en tiempo real

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+
+import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs, 'es');
+registerLocaleData(localeFr, 'fr');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    {
+      provide: LOCALE_ID,
+      useValue: 'es',
+    }
   ]
 };
--- src/app/pages/basic-page/basic-page.ts
+++ src/app/pages/basic-page/basic-page.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+import { LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe } from "@angular/common";
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.html',
 })
-export default class BasicPage { }
+export default class BasicPage { 
+  nameLower = signal('victor');
+  nameUpper = signal('VICTOR');
+  fullName = signal('viCtOr CaNo');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleaunp) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date);
+    }, 1000);
+
+    onCleaunp(() => {
+    clearInterval(interval);
+    })
+  });
+}
